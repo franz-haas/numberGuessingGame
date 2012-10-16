f00f211a0decf5f36e72b102214c0168a5254adc
Franz Haas <franz_haas@lavabit.com>
2012-10-16 21:29:56 +0200

    ai code roughedised

--- ng_ai_handling.js
+++ ng_ai_handling.js
@@ -1,7 +1,27 @@
 function ai_step(){
-  var g_c1 = 50;
-  var g_c2 = 50;
+  var g_c1 = "unset";
+  var g_c2 = "unset";
 
-  eval(window.document.NumberGuessing.user_algo.value);
+  var c1 = "dont cheat"
+  var c2 = "dont cheat"
+  
+  try{
+    eval(window.document.NumberGuessing.user_algo.value);
+  }catch (e){
+    alert("your code did not run")
+    alert(e.message)
+    return;
+  }
+
+  if(isNaN(g_c1)){
+    alert("g_c1 is not set correctly by your code");
+    return;
+  }
+    
+  if(isNaN(g_c2)){
+    alert("g_c2 is not set by your code");
+    return;
+  }
+
   workGuessWorker(g_c1, g_c2);
 }
